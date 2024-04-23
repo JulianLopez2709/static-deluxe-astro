4bb787e8da660bf2134c5b17dac9fa63f2e6cf4e
JulianLopez2709 <judalomor27.95@gmail.com>
2024-04-23 15:51:08 -0600

    first commit

---
+++ src/utils/menu.js
@@ -0,0 +1,18 @@
+export const list = [
+    {
+       path:"/#home",
+       label:"Home" 
+    },
+    {
+        path:"/#about",
+       label:"Sobre Nosotros" 
+    },
+    {
+        path:"/#location",
+       label:"Sucursales" 
+    },
+    {
+        path:"/#contact",
+       label:"Contacto" 
+    }
+]
